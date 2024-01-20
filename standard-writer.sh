2f593083581a352a3e36e087e3ff4070fd6a26ed
shigaichi <102985195+shigaichi@users.noreply.github.com>
2024-01-21 00:52:16 +0900

    Run with past dates to recover from past failures (#7)

---
+++ standard-writer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$1" = "-date" ] && [ -n "$2" ]; then
+    if ! date -d "$2" &> /dev/null; then
+        echo "Invalid date format: $2"
+        exit 1
+    fi
+    target_date=$(date -d "$2" '+%Y-%m-%d')
+else
+    target_date=$(date '+%Y-%m-%d')
+fi
+
+for i in {6..0}; do
+    day_to_process=$(date -d "$target_date - $i days" '+%Y-%m-%d')
+    ./writer -date "$day_to_process"
+done
